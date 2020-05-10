bb10517d166e2f9e589eaff064c24994e1917a6e
wangyu <1903372529@qq.com>
2020-05-10 14:53:00 +0800

    初始化

---
+++ src/api/user.js
@@ -0,0 +1,61 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/user/login',
+    method: 'post',
+    data
+  })
+}
+
+export function register(data) {
+  return request({
+    url: '/user/register',
+    method: 'post',
+    data
+  })
+}
+
+export function getInfo() {
+  return request({
+    url: '/user/getInfo',
+    method: 'get'
+  })
+}
+
+export function getAll() {
+  return request({
+    url: '/user/getAll',
+    method: 'get'
+  })
+}
+
+export function remove(data) {
+  return request({
+    url: '/user/remove',
+    method: 'delete',
+    data
+  })
+}
+
+export function update(data) {
+  return request({
+    url: '/user/update',
+    method: 'put',
+    data
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/user/logout',
+    method: 'post'
+  })
+}
+
+export function getCodeImg() {
+  return request({
+    url: '/user/getCodeImg',
+    method: 'get'
+  })
+}
